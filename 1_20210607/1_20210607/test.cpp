3d980310a71c729b69c2e18491433b340c76987d
240428037 <240428037@qq.com>
2021-06-07 22:36:51 +0800

    提交代码

---
+++ 1_20210607/1_20210607/test.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+	int getmax(int a, int b){
+		return a > b ? a : b;
+	}
+	int maxSubArray(vector<int>& nums) {
+		int sum = 0;
+		int max = nums[0];
+		for (int i = 0; i < nums.size(); i++){
+			sum = getmax(sum + nums[i], nums[i]);
+			if (sum >= max){
+				max = sum;
+			}
+		}
+		return max;
+	}
+};
